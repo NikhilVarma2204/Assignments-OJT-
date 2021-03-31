1f1230cf70a13a1ca960eb063692e8abb7730d64
NikhilVarma2204 <68849386+NikhilVarma2204@users.noreply.github.com>
2021-03-31 12:34:50 +0530

    Delete Assignment-1 directory

--- Assignment-1/ArraySorting/SearchArray.java
+++
@@ -1,8 +0,0 @@
-package ArraySorting;
-
-public class SearchArray {
-    public void searchArray(int[]arr,int toCheckValue){
-        boolean test= Arrays.asList(arr).contains(toCheckValue);
-        System.out.println(toCheckValue+"is present in"+test);
-    }
-}
--- Assignment-1/PromotedOrNot/Assignmnet1Q4.java
+++
@@ -1,8 +0,0 @@
-package Assignment1.PromotedOrNot;
-
-public class Assignmnet1Q4 {
-    public static void main(String[] args){
-        ResultDeclaration d= new ResultDeclaration();
-       d.declareResult(10,30,40);
-    }
-}
